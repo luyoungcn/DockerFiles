1d2fff8a530ab2497a5a86e030007f84966ca108
luyang <luyang>
2025-01-06 19:37:24 +0800

    update script and doc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# DockerFiles
-Dockerfile collections
+
+## 基于 Docker 构建 docker 镜像
+
+```bash
+docker build -t ubuntu1804:v1 .
+docker run --name workstation -di --privileged -v ~/01-coding:/home/deploy/01-coding ubuntu1804:v1
+docker exec -it workstation /bin/bash
+```
--- init.sh
+++ init.sh
@@ -2,8 +2,7 @@
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -11,15 +10,21 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 
 
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
+
+
 echo \
+
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-
+sudo usermod -aG docker $USER
